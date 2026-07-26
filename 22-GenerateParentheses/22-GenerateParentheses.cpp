58f59eaa28aabc65f23e6470a16ac940e4a80539
anwesh2006 <anweshpatro9@gmail.com>
2026-07-26 16:02:42 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/26/2026, 4:02:42 PM
+class Solution {
+public:
+    void backtrack(string curr, int open , int close , int n, vector<string> &res){
+        if(curr.size() == 2*n){
+            res.push_back(curr);
+            return;
+        }
+        if(open < n) backtrack(curr +'(' , open+1, close,n,res);
+        if(close<open) backtrack(curr +')' , open,close+1,n,res);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        backtrack("",0,0,n,res);
+        return res;
+    }
+};
