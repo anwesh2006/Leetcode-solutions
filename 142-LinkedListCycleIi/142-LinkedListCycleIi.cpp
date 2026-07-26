39a165274c39e22f377b96d161ab967e02ed98a2
anwesh2006 <anweshpatro9@gmail.com>
2026-07-26 16:02:01 +0530

    Create 142-LinkedListCycleIi

---
+++ 142-LinkedListCycleIi/142-LinkedListCycleIi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/26/2026, 4:02:01 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode * slow = head;
+        ListNode * fast = head;
+        while(slow && fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+                }
+                return slow;
+        }
+        }
+        
+        
+        return nullptr;
+    }
+};
